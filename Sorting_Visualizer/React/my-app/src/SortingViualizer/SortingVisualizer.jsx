7a864c338805fd34c9b8f4b25c40b4e444961222
Jawad TP <jawadtp248@gmail.com>
2022-08-07 20:03:20 +0530

    v1.0

---
+++ Sorting_Visualizer/React/my-app/src/SortingViualizer/SortingVisualizer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './SortingVisualizer.css'
+
+function randomIntFromInterval(min, max) {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+export default function SortingVisualizer (){
+return(<>
+
+</>)
+}
