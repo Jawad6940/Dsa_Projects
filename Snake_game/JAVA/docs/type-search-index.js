ba81c29e0ba8979b4e2f574e979b46a3663e5f3d
Jawad TP <jawadtp248@gmail.com>
2022-09-22 13:41:46 +0530

    v.1.0

---
+++ Snake_game/JAVA/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"App","l":"App"},{"p":"App","l":"GameFrame"},{"p":"App","l":"GamePanel"},{"p":"App","l":"GamePanel.MyKeyAdapter"}]
